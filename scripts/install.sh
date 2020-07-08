217358eb8b145036e18ddef2c6818f2f74466eda
Diego Frei <diego.frei@gmail.com>
2020-07-08 16:16:37 +0000

    Share initial script

---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y build-essential
+
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+sudo sh cuda_10.2.89_440.33.01_linux.run
+
+
